80a7fd96325815a88daaf1b7119b2d3da9f19ea1
Guillermo <guilleballarin19@gmail.com>
2022-10-25 20:58:45 +0200

    ..

--- React/part/src/App.js
+++ React/part/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.s</code> and save to reload.
+          Edit <code>src/App.sljdjhojhd</code> and save to reload.
         </p>
         <a
           className="App-link"
